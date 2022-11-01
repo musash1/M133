46cb9bdd584330adc4711a3e73cb11037ee56514
ruuiisu <luis.gjokaj@gmail.com>
2022-11-01 12:32:03 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+import { ToDo } from "./todo.js";
+
+let todos = [
+  new ToDo("Zugticket kaufen", false),
+  new ToDo("Wäsche waschen", true),
+  new ToDo("Hausaufgaben machen", true),
+];
+
+function updateToDoListOnScreen() {
+  const todoListElement = document.getElementById("todolist");
+  const todoListInput = document.getElementById("neuesToDo");
+
+  // Liste leeren
+  todoListElement.innerHTML = "";
+
+  // ToDo's einfügen
+  for (const todo of todos) {
+    const toDoListEntry = todo.element();
+    todoListElement.appendChild(toDoListEntry);
+  }
+
+  // offene ToDo's
+  const offeneToDos = todos.filter((offen) => !offen.erledigt);
+  const elementAnzahl = document.getElementById("anzahl");
+  elementAnzahl.textContent = `${offeneToDos.length} ToDo's offen`;
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  updateToDoListOnScreen();
+});
+
+document.addEventListener("keypress", (event) => {
+  let newTodo = document.getElementById("neuesToDo");
+  if (event.key === "Enter" && newTodo.value != "") {
+    todos.unshift(new ToDo(newTodo.value, false));
+    updateToDoListOnScreen();
+    newTodo.value = "";
+  }
+});
+
+document.getElementById("aufraeumen").addEventListener("click", () => {
+  let newTodos = [];
+  for (const todo of todos) {
+    if (!todo.erledigt) {
+      newTodos.push(todo);
+    }
+  }
+  todos = newTodos;
+  updateToDoListOnScreen();
+});
